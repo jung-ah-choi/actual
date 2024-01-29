3bf189f6707ecd871069023214be97616b4f4bce
jamie-jungah <wjddk718@gmail.com>
2024-01-29 20:57:54 +0900

    Feat: 음식점 리스트 pagination

--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -1,8 +1,28 @@
 import 'package:actual/restaurant/component/restaurant_card.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+
+import '../../common/const/data.dart';
 
 class RestaurantScreen extends StatelessWidget {
   const RestaurantScreen({super.key});
+
+  Future<List> paginateRestaurant() async {
+    final dio = Dio();
+
+    final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
+
+    final resp = await dio.get(
+      'http://$ip/restaurant',
+      options: Options(
+        headers: {
+          'authorization': 'Bearer $accessToken',
+        },
+      ),
+    );
+
+    return resp.data['data'];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,17 +30,40 @@ class RestaurantScreen extends StatelessWidget {
       child: Center(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: RestaurantCard(
-            image: Image.asset(
-              'asset/img/food/ddeok_bok_gi.jpg',
-              fit: BoxFit.cover,
-            ),
-            name: '달떡볶이',
-            tags: ['떡볶이', '치즈', '매운맛'],
-            ratingCount: 100,
-            deliveryTime: 15,
-            deliveryFee: 2000,
-            rating: 4.5,
+          child: FutureBuilder(
+            future: paginateRestaurant(),
+            builder: (context, AsyncSnapshot<List> snapshot) {
+              if (!snapshot.hasData) {
+                return Container();
+              }
+
+              return ListView.separated(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (_, index) {
+                  final item = snapshot.data![index];
+
+                  return RestaurantCard(
+                    image: Image.network(
+                      'http://$ip${item['thumbUrl']}',
+                      fit: BoxFit.cover,
+                    ),
+                    // image: Image.asset(
+                    //   'asset/img/food/ddeok_bok_gi.jpg',
+                    //   fit: BoxFit.cover,
+                    // ),
+                    name: item['name'],
+                    tags: List<String>.from(item['tags']),
+                    ratingsCount: item['ratingsCount'],
+                    deliveryTime: item['deliveryTime'],
+                    deliveryFee: item['deliveryFee'],
+                    ratings: item['ratings'],
+                  );
+                },
+                separatorBuilder: (_, index) {
+                  return SizedBox(height: 16.0);
+                },
+              );
+            },
           ),
         ),
       ),
